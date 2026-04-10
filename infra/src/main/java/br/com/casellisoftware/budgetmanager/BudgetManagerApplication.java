2b57c136119b6013b8fe76c7620ec3510e70f446
victor <victordcporto@gmail.com>
2026-04-10 14:19:26 -0300

    Task 11

---
+++ infra/src/main/java/br/com/casellisoftware/budgetmanager/BudgetManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.casellisoftware.budgetmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetManagerApplication.class, args);
+	}
+
+}
